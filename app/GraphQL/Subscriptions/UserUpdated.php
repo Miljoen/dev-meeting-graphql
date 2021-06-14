e2f54741bd83786c3a917343fde1bfb5adba8de1
Yoeri Moens <yoerimoens@gmail.com>
2021-06-14 09:01:36 +0200

    Add UserUpdated Subscription

---
+++ app/GraphQL/Subscriptions/UserUpdated.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\GraphQL\Subscriptions;
+
+use Illuminate\Http\Request;
+use Nuwave\Lighthouse\Schema\Types\GraphQLSubscription;
+use Nuwave\Lighthouse\Subscriptions\Subscriber;
+
+class UserUpdated extends GraphQLSubscription
+{
+    public function authorize(Subscriber $subscriber, Request $request): bool
+    {
+        return true;
+    }
+
+    public function filter(Subscriber $subscriber, $root): bool
+    {
+        return true;
+    }
+}
